90048c72086007b637a0ac802947cabe5d1f5331
Bassam <kandelchy@gmail.com>
2018-02-22 17:15:34 -0500

    retreive user role

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,13 +12,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # Retreive access token and decode it using JWT then print it to the console
       token = request.env["omniauth.auth"]["credentials"]["token"]
       decoded_token = JWT.decode token, nil, false
-      puts "*****************"
-      puts decoded_token
-      puts "*****************"
+      # puts "*****************"
+      # puts decoded_token
+      # puts "*****************"
+
+      url = URI('http://52.170.88.55/Discovery.Auth.API/api/Token/GetSecurityToken')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = false
+      request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
+      data = {"UserName":"Bassam@NitorSSO.onmicrosoft.com","Password":"NI@2018123","ClientId":"8e59734f-420f-47bf-b8e7-f9d8415b4e4d"}
+      data_json = data.to_json
+      request.body = data_json
+      response = http.request(request)
+      # puts "*****************"
+      $role = response.body
+      # puts "*****************"
+
     else
       session['devise.azure_oauth2_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
+
+
+
   end
 
   def failure
